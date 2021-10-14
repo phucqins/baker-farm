2f329e184ee150c10132736edda7da642adfaf91
phucq.ins <greenlogitics@gmail.com>
2021-10-14 21:47:01 +0700

    New

---
+++ base.js
@@ -0,0 +1,7 @@
+const navBarHandle = () => {
+  let x = document.querySelector(".nav-list");
+  x.classList.toggle("active");
+};
+const handleClick = () => {
+  document.querySelector(".mymodal").setAttribute("style", "display:none");
+};
